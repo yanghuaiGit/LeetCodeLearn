224ea8e9ad6f6e4aa17ac0dd55e3b23893e45b1c
yanghuai <1355743969@qq.com>
2018-10-23 21:32:17 +0800

    回文字符串判断 使用链表分割一半 前面反转再与后面判断相等

--- src/main/java/com/example/List/PalindromeList.java
+++ src/main/java/com/example/List/PalindromeList.java
@@ -24,12 +24,15 @@ public class PalindromeList {
         }
         p = head;
         ListNode p2 = head.next;
+
         head.next = null;
+
+        //p作为前面一半的链表 p2作为后面一半的链表
         for(int i = 1; i < nodeNum/2;i++){ //将前半部分反转，一次改变一个指针。假如说有M个结点，那么有M-1个指针，循环M-1次。
             ListNode tmp = p2.next;
-            p2.next = p;
-            p = p2;
-            p2 = tmp;
+            p2.next = p;//p2的下一个节点指向p  即把后面的链表的第一个节点作为前面链表的第一个节点 即反转
+            p = p2;//此时再把p2这个链表赋值给p 也就是p作为前面一半的链表
+            p2 = tmp;//剩下的 即后面的作为p2 等下一个循环 将p2的第一个元素作为前面p的第一个元素 即 p2.next = p
         }
         if(nodeNum%2 != 0 && p2 != null){ //考虑奇数的情况，跳过最中间的元素。
             p2 = p2.next;
